ef9a5fe1f1daa8ed72a0fb52b4e3a1176644b6e2
MariamMasre <meremmasre@gmail.com>
2022-08-13 12:54:19 +0300

    add task 11-2

--- app.js
+++ app.js
@@ -1,59 +1,232 @@
+  
+  let title =document.getElementById('title');
+  let price =document.getElementById('price');
+  let taxes =document.getElementById('taxes');
+  let ads =document.getElementById('ads');
+  let discount =document.getElementById('discount');
+  let total =document.getElementById('total');
+  let count =document.getElementById('count');
+  let category =document.getElementById('category');
+  let submit =document.getElementById('submit');
+  
+  let mood = 'create';
 
-
-/*var x = 999;
-function Food_ID(){
-    return x++;
-}*/
-let submit = document.getElementById("submit ");
-let form = document.getElementById("form");
-let table = document.getElementById("table");
-//const allFood = [];
-var val =0;
-function Food(val,name,type,price){
-   this.id = val;
-   this.name = name;
-   this.type = type;
-   this.price = price;
-   this.render();
-}
-
-Food.prototype.render =function(){
-    let newRow = document.createElement ('tr'); 
-    table.appendChild(newRow);
-     let newId = document.createElement ('td'); 
-     newId.textContent =this.id ;
-     newRow.appendChild (newId) ; 
-     let newName = document.createElement ('td'); 
-     newName.textContent = this.name;
-     
-     newRow.appendChild(newName);
-      let newType = document.createElement ('td');
+  let tmp;
       
-      newType.textContent = this.type ; 
-      
-      newRow.appendChild (newType) ; 
-      let newPrice = document.createElement ('td') ;
-       newPrice.textContent = this.price;
-        newRow.appendChild(newPrice); 
-         newRow.classList.add('row');
+function getTotal(){
+    
+
+
+
+    if(price.value != ''){
+        
+       let result = (+price.value + +taxes.value +  +ads.value)- +discount.value;
+       total.innerHTML =result;
+       
+       total.style.background = '#040';
+    } else{
+        total.innerHTML = '';
+        total.style.background ='#red';
+    }
+
 }
 
-Food.id;
+ let dataPro = [];
+ 
 
-form.addEventListener('submit',handleSubmit);
-function handleSubmit(event){
-    event.preventDefault();
-let name = event.target.foodName.value;
-let type = event.target.foodType.value;
-let Price = event.target.Price.value;
+submit.onclick = function(){
+  
+let newPro ={
+    title:title.value.toLowerCase(),
+    price:price.value,
+    taxes:taxes.value,
+    ads:ads.value,
+    discount:discount.value,
+    total:total.innerHTML,
+    count:count.value,
+    category:category.value.toLowerCase(),
+}
+dataPro.push(newPro);
 
-let id = uniqueId();
-const newFood = new Food(id,name,type,Price);
-console.log(newFood);
+if (mood ==='create'){
+
+if(newPro.count > 1){
+    for (let i = 0; i < newPro.count; i++) {
+        dataPro.push(newPro);
+        
+    }
+}else{
+    dataPro.push(newPro);
+}
+}else{
+dataPro[ tmp] =newPro;
+mood ='create';
+submit.innerHTML ='Creat';
+count.style.display = 'block' ;
 }
 
-function uniqueId(){
 
-    val =Math.floor(10000 +Math.random()*90000);
-return val;
+clearData();
+showData();
 }
+
+
+
+
+function clearData(){
+title.value ='';
+price.value ='';
+taxes.value ='';
+ads.value ='';
+discount.value ='';
+total.innerHTML ='';
+count.value ='';
+category.value ='';
+
+}
+
+function showData(){
+    getTotal();
+ let table = ''; 
+ for(let i = 0; i < dataPro.length;i++){
+    table += `
+    <tr>
+        <td>${i}</td>
+        <td>${dataPro[i].title}</td>
+        <td>${dataPro[i].parse}</td>
+        <td>${dataPro[i].taxes}</td>
+        <td>${dataPro[i].ads}</td>
+        <td>${dataPro[i].discount}</td>
+        <td>${dataPro[i].total}</td>
+        <td>${dataPro[i].category}</td>
+        <td><button onclick="updateData(${i})" id="updat">update</button></td>
+        <td><button onclick="deleteData(${i})" id="delete">delete</button></td> 
+    </tr>
+    `;
+ }
+ 
+ document.getElementById('tbody').innerHTML = table;
+ let btnDelete =document.getElementById('deleteAll');
+ if(dataPro.length>0){
+btnDelete.innerHTML=`
+ <button onclick="deleteAll()" >delete All(${dataPro.length}) </button>`;
+ }else{
+    btnDelete.innerHTML=` `;
+ }
+}
+showData();
+
+
+function deleteData(i){
+  
+dataPro.splice(i,1);
+
+showData();
+}
+function deleteAll(){
+    
+    dataPro.splice(0);
+    showData();
+}
+
+
+function updateData(i){
+    
+title.value =dataPro[i].title;
+price.value = dataPro[i].parse;
+taxes.value =dataPro[i].taxes;
+ads.value = dataPro[i].ads;
+discount.value =dataPro[i].discount;
+getTotal();
+
+count.style.display ='';
+category.value = dataPro[i].category;
+submit.innerHTML ='Update';
+mood ='update';
+
+tem = i;
+scroll({
+ top :0  ,
+ 
+ behavior:'smooth' ,
+})
+
+}
+
+
+let searchMood = 'title';
+function getSearchMood(id){
+    let search = document.getElementById('search');
+if (id == 'searchTitle'){
+searchMood = 'title';
+
+
+}else{
+    searchMood = 'category';
+    
+}
+search.placeholder='Search By '+searchMood;
+search.focus();
+
+search.value ='';
+
+showData();
+}
+function searchData(value){
+    let table ='';
+if(searchMood == 'title'){
+
+    for (let i = 0; i < dataPro.length; i++) {
+        if(dataPro[i].title.includes(value.toLowerCase())){
+            table += `
+            <tr>
+                <td>${i}</td>
+                <td>${dataPro[i].title}</td>
+                <td>${dataPro[i].parse}</td>
+                <td>${dataPro[i].taxes}</td>
+                <td>${dataPro[i].ads}</td>
+                <td>${dataPro[i].discount}</td>
+                <td>${dataPro[i].total}</td>
+                <td>${dataPro[i].category}</td>
+                <td><button onclick="updateData(${i})" id="updat">update</button></td>
+                <td><button onclick="deleteData(${i})" id="delete">delete</button></td> 
+            </tr>
+            `
+
+            
+
+        }
+        
+    }
+
+}else{
+
+    for (let i = 0; i < dataPro.length; i++) {
+        if(dataPro[i].category.includes(value.toLowerCase())){
+            table += `
+            <tr>
+                <td>${i}</td>
+                <td>${dataPro[i].title}</td>
+                <td>${dataPro[i].parse}</td>
+                <td>${dataPro[i].taxes}</td>
+                <td>${dataPro[i].ads}</td>
+                <td>${dataPro[i].discount}</td>
+                <td>${dataPro[i].total}</td>
+                <td>${dataPro[i].category}</td>
+                <td><button onclick="updateData(${i})" id="updat">update</button></td>
+                <td><button onclick="deleteData(${i})" id="delete">delete</button></td> 
+            </tr>
+            `
+
+            
+
+        }
+        
+    }
+}
+document.getElementById('tbody').innerHTML=table;
+}
+
+
+
+
